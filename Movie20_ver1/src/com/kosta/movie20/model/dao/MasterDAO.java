442fb8f264bc3ac0061b05c20af97356ccc542e8
kosta <kosta@192.168.0.164>
2017-10-13 16:46:11 +0900

    MasterDAO

--- Movie20_ver1/src/com/kosta/movie20/model/dao/MasterDAO.java
+++ Movie20_ver1/src/com/kosta/movie20/model/dao/MasterDAO.java
@@ -1,5 +1,122 @@
 package com.kosta.movie20.model.dao;
 
-public class MasterDAO {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import com.kosta.movie20.model.vo.MovieVO;
+import com.kosta.movie20.model.vo.NoticeVO;
+import com.sun.corba.se.pept.transport.Connection;
+
+public class MasterDAO {
+	private static MasterDAO masterDAO = new MasterDAO();
+
+	private MasterDAO() {
+
+	}// con
+
+	public static MasterDAO getInstance() {
+		return masterDAO;
+	}// getInstance
+
+	public void closeAll(PreparedStatement pstmt, Connection con) throws SQLException {
+		if (pstmt != null)
+			pstmt.close();
+		if (con != null)
+			con.close();
+	}// closeAll
+
+	public void closeAll(ResultSet rs, PreparedStatement pstmt, Connection con) throws SQLException {
+		if (rs != null)
+			rs.close();
+		closeAll(pstmt, con);
+	}// closeAll
+
+	public ArrayList<NoticeVO> noticeList() {
+		ArrayList<NoticeVO> list = new ArrayList<NoticeVO>();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+		} finally {
+
+		}
+
+		return list;
+	}// noticeList
+
+	public void movieRegister(MovieVO mvo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+		} finally {
+
+		}
+	}// movieRegister
+
+	public void movieDelete(String mNo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+		} finally {
+
+		}
+	}// movieDelete
+
+	public void movieUpdate(String mNo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+		} finally {
+
+		}
+	}// movieUpdate
+
+	public void noticeWrite(NoticeVO nvo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+		} finally {
+
+		}
+	}// noticeWrite
+
+	public void noticeDelete(String nNo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+		} finally {
+
+		}
+	}// noticeDelete
+
+	public void noticeUpdate(String nNo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+		} finally {
+
+		}
+	}// noticeUpdate
 }
